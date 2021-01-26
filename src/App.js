77710a366f0b45594757395201743926c3ebb1a3
rlems <rene@renelems.nl>
2021-01-26 15:00:22 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,71 @@
+import { useEffect } from "react";
+import confetti from "canvas-confetti";
+import { Howl } from "howler";
+import hornSound from "./party_horn.mp3";
+import happyBdaySong from "./happybday.mp3";
+import "./styles.css";
+
+export default function App() {
+  useEffect(() => {
+    const sound = new Howl({
+      src: [hornSound],
+      volume: 0.3,
+      onend: () => {
+        setTimeout(() => {
+          sound.play();
+        }, 1000);
+      }
+    });
+    const bdaySong = new Howl({
+      src: [happyBdaySong],
+      volume: 0.7,
+      loop: true,
+      sprite: {
+        song: [10000, 128000, true]
+      }
+    });
+
+    bdaySong.play("song");
+    setTimeout(() => {
+      sound.play();
+    }, 1000);
+
+    function randomInRange(min, max) {
+      return Math.random() * (max - min) + min;
+    }
+
+    const interval = setInterval(() => {
+      confetti({
+        angle: randomInRange(55, 125),
+        spread: randomInRange(50, 70),
+        particleCount: randomInRange(50, 100),
+        origin: { y: 0.6 }
+      });
+    }, 1000);
+
+    return () => {
+      clearInterval(interval);
+      bdaySong.stop();
+      sound.stop();
+    };
+  }, []);
+
+  return (
+    <div className="App">
+      <div className="content">
+        <h1>Tillykke med fødselsdagen</h1>
+        <p>
+          Van harte gefeliciteerd, we wensen je een topdag toe! Ook hebben we
+          een cadeaubon van 250DKK voor je achtergelaten bij de Shoppen Liquor
+          Store (Ahornsgade 22). Je kan 'm daar zelf ophalen en besteden onder
+          jouw naam.
+        </p>
+        <p>
+          Liefs,
+          <br />
+          Quin & René
+        </p>
+      </div>
+    </div>
+  );
+}
